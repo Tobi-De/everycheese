0ebe425f36bea7f6a3c564a6508bc381f6a32e11
Tobi DEGNON <tobidegnon@protonmail.com>
2020-12-07 12:46:01 +0100

    Implement cheese list page, add navbar link

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
         include("everycheese.users.urls", namespace="users"),
     ),
     path("accounts/", include("allauth.urls")),
+    path(
+        "cheeses/",
+        include("everycheese.cheeses.urls", namespace="cheeses"),
+    ),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
@@ -51,6 +55,4 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [
-            path("__debug__/", include(debug_toolbar.urls))
-        ] + urlpatterns
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
